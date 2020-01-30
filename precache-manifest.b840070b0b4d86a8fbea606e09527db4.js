87b0c61ef6cd8ca056afb0bdc6aa5e8c6d28a5e2
chiranz <chiranpoudyal@gmail.com>
2020-01-30 09:40:40 +0530

    Updates

---
+++ precache-manifest.b840070b0b4d86a8fbea606e09527db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6eb93c3d6fce76df60ab340ffa3e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8554465fd78c3b3d967",
+    "url": "/static/css/main.40b07499.chunk.css"
+  },
+  {
+    "revision": "4e0821dc123033995036",
+    "url": "/static/js/2.82fe5871.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.82fe5871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8554465fd78c3b3d967",
+    "url": "/static/js/main.9a1448af.chunk.js"
+  },
+  {
+    "revision": "c0681a03549dfc9a1631",
+    "url": "/static/js/runtime-main.df839dcd.js"
+  }
+]);
